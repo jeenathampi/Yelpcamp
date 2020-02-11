fd573ea7d39587ac76ef1c8898c5e02bbd4a59f6
Jeena <jeenathampi.23@gmail.com>
2020-02-10 22:37:32 -0800

    added console.log

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds");
 var commentRoutes = require("./routes/comments");
 var indexRoutes = require("./routes/index");
 
-
+console.log(process.env.DBURL);
 mongoose.connect(process.env.DBURL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(bodyParser.urlencoded({extended:true}));
